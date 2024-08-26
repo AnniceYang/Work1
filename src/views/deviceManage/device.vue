7671c670157af0d7e7128d807518060dc6e33dd4
Annice <annicelesy@gmail.com>
2024-08-26 10:59:13 +0800

    26082024定版正式

--- src/views/deviceManage/device.vue
+++ src/views/deviceManage/device.vue
@@ -336,12 +336,12 @@
                 <el-button type="text" @click="handleStatistics(scope.row)">{{
                   $t("deviceManage.details")
                 }}</el-button>
-                <!-- <el-button
-                type="text"
-                @click="handleCellSet(scope.row)"
-                v-if="permissions.admin_lsydevice_edit"
-                >{{ $t("deviceManage.batterySettings") }}</el-button
-              > -->
+                <el-button
+                  type="text"
+                  @click="handleCellSet(scope.row)"
+                  v-if="isAdmin"
+                  >{{ $t("deviceManage.batterySettings") }}</el-button
+                >
                 <el-button
                   type="text"
                   @click="handleUpgrade(scope.row)"
